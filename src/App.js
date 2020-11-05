88188e23fdeaa6bc1977acded61721e8d9a459c6
Methkal <methkal.khalawi@gmail.com>
2020-11-05 22:47:03 +0100

    the project with CRA full featured and dockerized

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import HomePage from "./pages/home-page/home-page.component";
+import DetailsPage from './pages/details-page/details-page.component';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { Route, Switch } from 'react-router-dom'
+
+const App = () => {
+    return (
+      <Switch>
+        <Route exact path="/pokemon/:pokimonid" component={DetailsPage} />
+        <Route path="/" component={HomePage} />
+      </Switch>
+    );
 }
-
 export default App;
